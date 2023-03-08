5164958bc664c1ce3480e0e0b3d6eafb08127df1
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-08 20:27:14 +0430

    Add constrauctor for Label class'

--- classes/label.rb
+++ classes/label.rb
@@ -2,6 +2,13 @@ class Lable
     attr_accessor :title :color
     attr_reader :items :id
 
+    def initialize (title, color)
+        @id = Random.rand(1..1000)
+        @title = title
+        @color = color
+        @items = []
+    end
+
    
 
 end
