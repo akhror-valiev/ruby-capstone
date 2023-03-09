c3dbabe753c20f976778eb793d33fd7918d1bd7a
Zuhra Hashimi <zuhrahashimi2001@gmail.com>
2023-03-09 19:06:01 +0430

    Create App class with methood

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'fileutils'
+require 'json'
+require_relative './classes/book_service'
+require_relative './classes/game_service'
+
+class App
+
+    def initialize
+        @books = BookService.new
+        @games = GameService.new
+        @items = []
+    end
+
+    
+  def data(directory, filename)
+    FileUtils.mkdir_p(directory)
+    file_path = "./#{directory}/#{filename}"
+    File.exist?(file_path) && JSON.parse(File.read(file_path))
+  end
+end
